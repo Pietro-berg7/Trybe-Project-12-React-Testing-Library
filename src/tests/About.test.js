cf7b490ad3508c3fea502c756db63b7035ceafee
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-10-22 01:10:55 -0300

    Requisito 2 concluído

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+describe('Testes do componente About.js', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+
+    const about = screen.getByRole(
+      'heading',
+      { name: 'About Pokédex' },
+    );
+    expect(about).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    render(<About />);
+
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
