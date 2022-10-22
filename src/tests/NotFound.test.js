f2464eaf505d8af747b80c512d68a4938c31c0c4
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-10-22 01:32:47 -0300

    Requisito 4 concluído

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testes do componente NotFound.js', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const notFound = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found' },
+    );
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
